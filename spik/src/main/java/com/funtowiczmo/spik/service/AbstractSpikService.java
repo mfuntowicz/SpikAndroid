0ce9da414e058c537cb2e638b380dec0c9c40868
Morgan Funtowicz <funtowiczmo@gmail.com>
2015-11-02 22:40:10 +0100

    Abstract implementation of the Spik Service. Provide essential functionality such as setting up the service, showing notification, etc.

--- spik/src/main/java/com/funtowiczmo/spik/service/AbstractSpikService.java
+++ spik/src/main/java/com/funtowiczmo/spik/service/AbstractSpikService.java
@@ -2,13 +2,18 @@ package com.funtowiczmo.spik.service;
 
 import android.app.*;
 import android.content.Intent;
+import android.graphics.drawable.Icon;
 import android.os.Binder;
 import android.os.IBinder;
+import android.support.annotation.Nullable;
 import com.funtowiczmo.spik.ConnectionActivity;
 import com.funtowiczmo.spik.R;
 import com.funtowiczmo.spik.lang.Computer;
+import com.funtowiczmo.spik.lang.Phone;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Created by momo- on 27/10/2015.
@@ -22,77 +27,36 @@ public abstract class AbstractSpikService extends Service {
 
     /** System resources **/
     private NotificationManager notificationManager;
-    private final IBinder mBinder = new SpikBinder();
 
     /** Spik resouces **/
-    private Computer computer;
-
-    protected AbstractSpikService() {}
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        return mBinder;
-    }
+    private final AtomicBoolean isInForeground = new AtomicBoolean(false);
 
     @Override
     public void onCreate() {
+        LOGGER.info("Creating Service");
         notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
     }
 
     @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return Service.START_NOT_STICKY;
+    }
+
+    @Override
     public void onDestroy() {
-       onDisconnected();
+        LOGGER.info("Destroying Service");
+        stopSpik();
     }
 
-    /**
-     * Called when the service is connected to a computer
-     * @param computer The computer the device is connected to
-     */
-    protected void onConnected(Computer computer){
-        LOGGER.info("Connected to {}", computer);
-
-        this.computer = computer;
-        showConnectedNotification();
-    }
-
-    /**
-     * Called when the service is disconnect from a computer
-     */
-    protected void onDisconnected(){
-        LOGGER.info("Disconnected from {}", computer);
-
-        final Computer ref = computer;
-
-        dispose();
-        showDisconnectedNotification(ref);
-    }
-
-    /**
-     * Show a notification about the connection to a device
-     */
-    private void showConnectedNotification(){
-        PendingIntent contentIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), new Intent(this, ConnectionActivity.class), 0);
-
-        Notification notification = new Notification.Builder(this)
-                .setContentTitle(getResources().getString(R.string.app_name))
-                .setContentText(getResources().getString(R.string.local_service_started, computer.name()))
-                .setTicker(computer.name())
-                .setWhen(System.currentTimeMillis())
-                .setSmallIcon(R.drawable.ic_launcher)
-                .setContentIntent(contentIntent)
-                .setAutoCancel(true)
-                .build();
-
-        //Flag : RUNNING | NO CLEAR
-        notification.flags = Notification.FLAG_ONGOING_EVENT | Notification.FLAG_NO_CLEAR;
-        notificationManager.notify(CONNECTED_NOTIFICATION_ID, notification);
-    }
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) { return null; }
 
     /**
      * Show a notification about the disconnection
      * @param c
      */
-    private void showDisconnectedNotification(Computer c){
+    protected void showDisconnectedNotification(Computer c){
         PendingIntent contentIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), new Intent(this, ConnectionActivity.class), 0);
 
         Notification notification = new Notification.Builder(this)
@@ -108,23 +72,33 @@ public abstract class AbstractSpikService extends Service {
         notificationManager.notify(DISCONNECTED_NOTIFICATION_ID, notification);
     }
 
-    /**
-     * Free all resource used by this service
-     */
-    private void dispose(){
-        notificationManager.cancel(CONNECTED_NOTIFICATION_ID);
-        computer = null;
+    protected void launchSpik(Computer computer){
+        LOGGER.info("Starting Spik Service");
+
+        if(isInForeground.compareAndSet(false, true)) {
+            PendingIntent contentIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), new Intent(this, ConnectionActivity.class), 0);
+
+            Notification notification = new Notification.Builder(this)
+                    .setContentTitle(getResources().getString(R.string.app_name))
+                    .setContentText(getResources().getString(R.string.local_service_started, computer.name()))
+                    .setTicker(computer.name())
+                    .setWhen(System.currentTimeMillis())
+                    .setSmallIcon(R.drawable.ic_launcher)
+                    .setContentIntent(contentIntent)
+                    .setAutoCancel(true)
+                    .build();
+
+            notification.flags = Notification.FLAG_NO_CLEAR;
+
+            startForeground(CONNECTED_NOTIFICATION_ID, notification);
+        }
     }
 
-    /** Local Spik Service Binder **/
-    public class SpikBinder extends Binder {
-
-        /**
-         * Return the service
-         * @return
-         */
-        AbstractSpikService getService() {
-            return AbstractSpikService.this;
+    protected void stopSpik(){
+        LOGGER.info("Stopping Spik Service");
+        if(isInForeground.compareAndSet(true, false)){
+            stopForeground(true);
+            stopSelf();
         }
     }
 }
